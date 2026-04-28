b3f20bdb0322d2fe69cc5f44d465f183292c4f05
natta1995 <natalie.hallerdal@yahoo.se>
2026-04-28 14:40:03 +0200

    Adding more Dtos and adding them to API

---
+++ QuestGameDetective.API/Dtos/Quest/QuestReadDto.cs
@@ -0,0 +1,13 @@
+﻿using QuestGameDetective.API.Models.Enums;
+
+public class QuestReadDto
+{
+    public Guid Id { get; set; }
+
+    public QuestStatus Status { get; set; }
+    public QuestResult Result { get; set; }
+
+    public DateTime? AcceptedAt { get; set; }
+
+   // public DateTime? CompletedAt { get; set; }
+}
