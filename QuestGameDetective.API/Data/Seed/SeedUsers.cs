c127754dc7ad7d272c4704eaa79f6374240ff628
natta1995 <natalie.hallerdal@yahoo.se>
2026-04-29 11:40:07 +0200

    data - seedusers

---
+++ QuestGameDetective.API/Data/Seed/SeedUsers.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Identity;
+using QuestGameDetective.API.Models;
+
+namespace QuestGameDetective.API.Data
+{
+    public static class SeedUsers
+    {
+        public static async Task SeedAsync(IServiceProvider services)
+        {
+            var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
+
+            var adminEmail = "admin@test.com";
+            var userEmail = "user@test.com";
+
+            if (await userManager.FindByEmailAsync(adminEmail) == null)
+            {
+                var admin = new ApplicationUser
+                {
+                    UserName = "AdminPlayer",
+                    Email = adminEmail
+                };
+
+                var result = await userManager.CreateAsync(admin, "Admin123!");
+
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(admin, "Admin");
+                }
+            }
+
+            if (await userManager.FindByEmailAsync(userEmail) == null)
+            {
+                var user = new ApplicationUser
+                {
+                    UserName = "UserPlayer",
+                    Email = userEmail
+                };
+
+                var result = await userManager.CreateAsync(user, "User123!");
+
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(user, "User");
+                }
+            }
+        }
+    }
+}
