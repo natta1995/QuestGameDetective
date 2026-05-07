c17810d40f582ea1c7bde3d01ee66e7680c69d4d
natta1995 <natalie.hallerdal@yahoo.se>
2026-05-07 16:58:21 +0200

    Remake of API result (put) , To make it more usefull for frontend.

---
+++ QuestGameDetective.Application/Dtos/Quest/AccuseSuspectResultDto.cs
@@ -0,0 +1,13 @@
+﻿using QuestGameDetective.Domain.Enums;
+
+
+namespace QuestGameDetective.Application.Dtos.Quest
+{
+    public class AccuseSuspectResultDto
+    {
+        public bool IsCorrect { get; set; }
+        public QuestResult Result { get; set; }
+
+        public string SolutionText { get; set; } = "";
+    }
+}
